fef2eb61ecd46558e1b3159be3e6ea13f81b38c6
Mohammad Rahimi <moyi.pary@gmail.com>
2023-01-31 13:37:01 +0330

    till part1 - 5.DjangoORM - ep 18

--- store/views/imports.py
+++ store/views/imports.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render, redirect
+from django.db.models.aggregates import Count, Sum
 
 from store.models import *
--- store/views/index.py
+++ store/views/index.py
@@ -2,6 +2,9 @@ from .imports import *
 
 
 def index(request):
-    products = Product.objects.all().filter(id__range=(1, 4))
-    orders = Order.objects.filter(customer_id=1)
-    return render(request, f'{__name__.replace(".", "/")}.html', {'products': products, 'orders': orders})
+    # products = Product.objects.all().select_related('collection')
+    orders = Order.objects.order_by('-placed_at').select_related('customer').prefetch_related('orderitem_set__product')[:5]
+    order_count = Customer.objects.filter(id=1)[0].order_set.all()[0].orderitem_set.count()
+    order_count = Customer.objects.filter(id=1)[0].order_set.all().prefetch_related('orderitem_set').aggregate(Count('orderitem_set__id'))
+    print(order_count)
+    return render(request, f'{__name__.replace(".", "/")}.html', {'orders': orders})
